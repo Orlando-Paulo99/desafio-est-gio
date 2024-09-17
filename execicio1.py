b23726cf01a7f58c485fe076e425cd3c44658fad
orlando paulo <rlandopaulo817@gmail.com>
2024-09-17 18:44:34 -0300

    exercicio1

---
+++ execicio1.py
@@ -0,0 +1,13 @@
+''' 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA? '''
+
+indice,SOMA,K=(13,0,0)
+
+while K < indice:
+    K=K+1
+    SOMA=SOMA+K
+   
+  
+print(SOMA)
